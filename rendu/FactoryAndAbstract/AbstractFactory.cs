c065b86627009fcc803bc8e4a9632d0ad1ebc4b5
loic-roux-404 <loic.roux.404@gmail.com>
2022-07-06 18:27:27 +0200

    factory

---
+++ rendu/FactoryAndAbstract/AbstractFactory.cs
@@ -0,0 +1,84 @@
+﻿
+namespace AbstractFactory {
+
+    public interface IAutomobileSpecFactory
+    {
+        IBluetooth createBluetoothSystem();
+    }
+
+    public interface IBluetooth
+    {
+        string Name { get; }
+        // Could imagine properties of bluetooth
+    }
+
+    class V4Bluetooth : IBluetooth
+    {
+        public string Name { get; } = "v4Bluetooth";
+        // Could imagine properties of bluetooth
+    }
+
+    class V3Bluetooth : IBluetooth
+    {
+        public string Name { get; } = "v3Bluetooth";
+    }
+
+    public class Automobile
+    {
+        protected string model;
+        protected string color;
+        protected string space;
+        protected double power;
+        protected IBluetooth bluetoothSystem;
+
+        public Automobile(
+        string model,
+        string color,
+        string space,
+        double power,
+        IBluetooth bluetoothSystem
+        ) {
+            this.model = model;
+            this.color = color;
+            this.space = space;
+            this.power = power;
+            this.bluetoothSystem = bluetoothSystem;
+        }
+
+        public override string? ToString() {
+            return "{" + 
+                this.model + 
+                this.color + 
+                this.space + 
+                this.power + 
+                this.bluetoothSystem.Name + 
+            "}";
+        }
+    }
+
+    class ElectricCarBluetoothFactory : IAutomobileSpecFactory
+    {
+        public IBluetooth createBluetoothSystem()
+        {
+            return new V3Bluetooth();
+        }
+    }
+
+    class SportCarBluetoothFactory : IAutomobileSpecFactory
+    {
+
+        public IBluetooth createBluetoothSystem()
+        {
+            return new V4Bluetooth();
+        }
+    }
+
+    // factory method using abstract factory
+    public class AutomobileFactory
+    {
+        public Automobile create(string model, double power, string color, string space, IAutomobileSpecFactory bluetoothFactory)
+        {
+            return new Automobile(model, color, space, power, bluetoothFactory.createBluetoothSystem());
+        }
+    }
+}
---
+++ rendu/FactoryAndAbstract/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace AbstractFactory
+{
+
+    class Program {
+        public static void Main(string[] args)
+        {
+            var mercedesAmg = new AutomobileFactory()
+                .create(
+                    "amg",
+                    100D,
+                    "#3386FF",
+                    "",
+                    new SportCarBluetoothFactory()
+                );
+
+            Console.WriteLine(mercedesAmg);
+        }
+    }
+}
